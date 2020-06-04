9ec89ff92e7999e35de46bbb7eb4a0dfa0c65b25
= <sh.awais786@gmail.com>
2020-06-04 05:07:06 +0500

    Lec 2 practice message added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Owais Ishfaq practiced content of second lecture, bootcamp2020.
         </p>
         <a
           className="App-link"
